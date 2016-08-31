ca2ad5b93523c555aefebe68ae50fd1dc6a339d9
HappyDeng <jzhou_deng@163.com>
2016-08-31 18:24:52 +0800

    添加登录页面

--- src/com/maxplus/sostudy/activity/LoginActivity.java
+++ src/com/maxplus/sostudy/activity/LoginActivity.java
@@ -1,29 +1,21 @@
 package com.maxplus.sostudy.activity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
 import com.maxplus.sostudy.R;
-import com.maxplus.sostudy.controller.LoginController;
-import com.maxplus.sostudy.view.LoginView;
+//import com.maxplus.sostudy.view.LoginView;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends Activity {
 
-    private LoginView mLoginView = null;
-    private LoginController mLoginController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mLoginView = (LoginView) findViewById(R.id.login_view);
-        mLoginView.initModule();
-        mLoginController = new LoginController(mLoginView, this);
-        mLoginView.setListener(mLoginController);
-        mLoginView.setListeners(mLoginController);
-        mLoginView.setOnCheckedChangeListener(mLoginController);
-        Intent intent = this.getIntent();
-        mLoginView.isShowReturnBtn(intent.getBooleanExtra("fromSwitch", false));
+
     }
 
     @Override
@@ -45,29 +37,6 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-    }
-
-    @Override
-    public void onBackPressed() {
-        finish();
-        super.onBackPressed();
-    }
-
-    public Context getContext() {
-        return this;
-    }
-
-    public void startMainActivity() {
-        Intent intent = new Intent();
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        intent.setClass(getContext(), MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void startRegisterActivity() {
-        Intent intent = new Intent();
-        intent.setClass(this, RegisterActivity.class);
-        startActivity(intent);
     }
 
 }
